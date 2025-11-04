775e455cefe8575759aceda90b8da594a1c2b0fb
arjunaarr <arjunanurarrafi@gmail.com>
2025-11-04 10:58:03 +0700

    UTS sisi klien

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,36 +1,22 @@
-import React, { useEffect, useState } from "react";
-import UserCard from "./UserCard";
-import { getUsers } from "./Services"
+import React from "react";
+import PostCard from "./PostCard";
+import { postsData } from "./data/postsData";
 
-function Exercise() {
-  const [users, setUsers] = useState([]);
-
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const data = await getUsers();
-      setUsers(data);
-    } catch (error) {
-      console.error("[Component] Gagal menampilkan data:", error.message);
-    }
-  };
-  fetchData();
-}, []);
-
-    return (
-    <>
-      <div className="min-h-screen bg-gray-100 p-6">
-        <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
-          User Cards
+const Exercise = () => {
+  return (
+    <div className="min-h-screen bg-special-mainBg p-8">
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-2xl font-semibold text-red-600 mb-8 text-center">
+          Post Cards
         </h1>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-         {users.map((user) => (
-            <UserCard key={user.email} {...user} />
-         ))} 
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
+          {postsData.map((post) => (
+            <PostCard key={post.id} {...post} />
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default Exercise;
